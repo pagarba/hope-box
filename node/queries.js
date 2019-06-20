262ee7d308cea6351e12e3ab68878a012775b835
Andrew <andrewchoi1@hotmail.com>
2019-06-20 14:54:16 -0400

    added db password

--- node/queries.js
+++ node/queries.js
@@ -8,6 +8,7 @@ const pool = new Pool({
     user: process.env.USER,
     host: process.env.HOST,
     database: process.env.DATABASE,
+    password: process.env.PASSWORD,
     port: process.env.PORT
 })
 
