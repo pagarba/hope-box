a2f30cda09b1333952b63a7375940449591d6a97
Andrew <andrewchoi1@hotmail.com>
2019-06-20 16:49:11 -0400

    changed build path

--- node/index.js
+++ node/index.js
@@ -5,6 +5,6 @@ const path = require('path');
 
 app.get('/data/fetch', data.fetch_data)
 
-app.use(express.static(path.join(__dirname, 'client/build')))
+app.use(express.static(path.join(__dirname, '../client/build')))
 
 app.listen(3500, () => console.log('listening on 3500'))
