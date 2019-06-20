edbf52c06924baa0419313666aa194cc52138a3a
Andrew <andrewchoi1@hotmail.com>
2019-06-20 12:26:29 -0400

    fixed table inputs

--- node/table.sql
+++ node/table.sql
@@ -7,7 +7,10 @@ CREATE TABLE data
 );
 
 INSERT INTO data
-    (ismi, lat, lon, message)
+    (imsi, lat, lon, message)
 VALUES
-    ('520031234567890', '51.5', '-0.096', 'Assistance'),
-    ('George', 'george@example.com');
+    ('502130123456789', '51.496,', '-0.096', 'connected'),
+    ('460001357924680', '51.50,', '-0.08', 'Need Help'),
+    ('470010171566423', '51.503', '-0.09', 'no assistance needed'),
+    ('520010171566423', '51.5', '-0.095', 'no help needed'),
+    ('310150123456789', '51.4969', '-0.087', 'device is connected');
