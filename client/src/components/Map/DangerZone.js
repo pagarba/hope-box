612f846413081cddd98e093e95bf9e397d096328
Andrew <andrewchoi1@hotmail.com>
2019-06-25 15:31:51 -0400

    changed shape of danger zone added data

--- client/src/components/Map/DangerZone.js
+++ client/src/components/Map/DangerZone.js
@@ -2,7 +2,7 @@ import React from 'react';
 import { Polygon } from 'react-leaflet';
 
 const DangerZone = () => {
-  const polygon = [[34.0495, -118.2312], [34.0285 , -118.2320], [34.0485, -118.2570]]
+  const polygon = [[34.0495, -118.2312], [34.0290, -118.2267], [34.0485, -118.2560]]
   return (
       <div>
         <Polygon color="red" positions={polygon} />
@@ -11,3 +11,7 @@ const DangerZone = () => {
 }
 
 export default DangerZone;
+
+
+
+
