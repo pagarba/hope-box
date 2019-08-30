069b7ca8d539ded5d6b8bcf11fc127140913a69a
dustinengle <dustin.engle@gmail.com>
2019-08-30 12:18:10 -0700

    typo fix for ussd bts script

--- bts/scripts/ussd.sh
+++ bts/scripts/ussd.sh
@@ -10,7 +10,7 @@ cp -f ussd/osmo-euse-demo.c build/osmo-hlr/src/osmo-euse-demo.c && \
 
 info "Building USSD demo gateway..." && \
 cd build/osmo-hlr/src/ && \
-make osmo-euse-demo >/dev.null && \
+make osmo-euse-demo >/dev/null && \
 
 info "Running USSD demo gateway..." && \
 ./osmo-euse-demo && \
